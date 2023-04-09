371e4e45041cd4ea4841f7f6170884f832f25b14
mehdi <amirabody.dev@gmail.com>
2023-04-09 23:28:44 +0330

    chore: create types declaration files in rollup build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,10 +6,19 @@ export default [
   {
     input: "src/cli.ts",
     output: {
-      file: "dist/index.mjs",
+      file: "dist/cli.mjs",
       format: "es",
       compact: true,
     },
     plugins: [typescript(), terser(), shebang({ include: ["**/*.ts"] })],
+  },
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.mjs",
+      format: "es",
+      compact: true,
+    },
+    plugins: [typescript({ declaration: true, declarationDir: "types", include: ["src/{index,utils}.ts"] }), terser()],
   },
 ];
